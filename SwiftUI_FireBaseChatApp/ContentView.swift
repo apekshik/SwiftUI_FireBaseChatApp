480c6c141c194f74a3325e4fbc58e3c2e1a324ee
Apekshik Panigrahi <apekshik@gmail.com>
2022-06-04 19:02:38 +0530

    Initial Commit

---
+++ SwiftUI_FireBaseChatApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_FireBaseChatApp
+//
+//  Created by Apekshik Panigrahi on 6/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_FireBaseChatApp/SwiftUI_FireBaseChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FireBaseChatAppApp.swift
+//  SwiftUI_FireBaseChatApp
+//
+//  Created by Apekshik Panigrahi on 6/4/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FireBaseChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
